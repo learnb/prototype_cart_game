d78f3281d44b6c02190c059eb90083c0f7ddf547
learnb <bryan@bryanlearn.com>
2020-02-03 20:06:49 -0500

    player movement and basic collision detection

--- Objects/Enemy/Enemy.gd
+++ Objects/Enemy/Enemy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -1,16 +1,58 @@
-extends Node2D
+extends KinematicBody2D
 
+onready var ray = $RayCast2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum PLAYER_STATES {
+	IDLE,
+	WALK,
+	ATTACK,
+	HOLDING,
+	PULLING
+}
 
+var state: int = PLAYER_STATES.IDLE
+var motion: Vector2 = Vector2()
+var speed: int = 80
+var rayVec: Vector2 = Vector2()
+var rayLength = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _physics_process(_delta):
+	get_input()
+	check_collision()
+	move_and_slide(self.motion)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_input():
+	# Detect up/down/left/right keystate and only move when pressed.
+	self.motion = Vector2()
+	self.rayVec = Vector2(0,0)
+
+	if Input.is_action_pressed('player_right'):
+		self.motion.x += 1
+		self.rayVec.x += rayLength
+	if Input.is_action_pressed('player_left'):
+		self.motion.x -= 1
+		self.rayVec.x -= rayLength
+	if Input.is_action_pressed('player_down'):
+		self.motion.y += 1
+		self.rayVec.y += rayLength
+	if Input.is_action_pressed('player_up'):
+		self.motion.y -= 1
+		self.rayVec.y -= rayLength
+	
+	ray.set_cast_to(self.rayVec)
+	self.motion = self.motion.normalized() * speed
+
+func check_collision():
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider.is_in_group("Enemy"):
+			print("Player hit Enemy")
+		elif collider.is_in_group("Pickup"):
+			print("Player hit Pickup")
+		elif collider.is_in_group("Cart"):
+			print("Player hit Cart")
+		else:
+			print("Player hit something else")
